7b1ee69631d3baafb6da7e9c9f51eab332cd025c
Chandru Suresh <sureshchandru2003@gmail.com>
2024-07-20 16:05:05 +0100

    change everything

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+import { Poppins } from "next/font/google";
+
+export const fontPoppins = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ]
+});
+
+export const metadata: Metadata = {
+  title: "Site Name",
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <body
+        className={clsx(
+          "min-h-screen font-sans antialiased",
+          fontPoppins.className,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center gap-4 py-8 md:py-10">
+      <Button>Hello World!</Button>
+    </div>
+  );
+}
