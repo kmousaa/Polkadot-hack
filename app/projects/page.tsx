ef607eaece5dcca0afc9e438e467bae3981df966
Chandru Suresh <sureshchandru2003@gmail.com>
2024-07-20 17:48:37 +0100

    fix: colors

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <p>kbsfbgkbjrgj</p>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "#040111",
+        background: "#1d1a29",
         primary: "#9d7cf9",
         secondary: "#7f079e",
         accent: "#f412a5",
@@ -25,14 +25,14 @@ module.exports = {
         dark: {
           colors: {
             primary: {
-              DEFAULT: "#9d7cf9",
+              DEFAULT: "#1d1a29",
               foreground: "#fbfaff",
             },
             secondary: {
               DEFAULT: "#7f079e",
               foreground: "#fbfaff",
             },
-            focus: "#9d7cf9",
+            focus: "#1d1a29",
           },
         },
       },
