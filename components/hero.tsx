2db8f931baca2d2639cd7d3f27a6e0c2cf095bd2
Rahman, Arraf (k21034926) <arrafrahman22@gmail.com>
2024-07-21 11:03:56 +0100

    text change

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,16 +15,16 @@ const Hero: React.FC = () => {
                 alt="NextUI Album Cover"
                 className="m-5"
             />
-            <div className="items-center justify-center z-40 space-y-16">
-                <h1 className="items-center justify-center font-bold text-9xl -ml-10 z-40 text-primary">
-                        Branchy_Dot
+            <div className="items-center justify-center z-40 space-y-16 text-right p-14">
+                <h1 className="items-center justify-center font-thin text-9xl -ml-10 z-40 text-secondary ">
+                    Journey 
                         
                 </h1>
-                <h1 className="items-center italic justify-center font-bold text-9xl -ml-10 z-40 pl-5 text-accent1 text-secondary" >
-                    Supporting Projects
+                <h1 className="items-center  justify-center font-thin text-7xl -ml-10 z-40 pl-5 text-accent1 text-primary font-" >
+                    Crowdfunding Projects
                     
                 </h1>
-                <h2 className="text-2xl text-center tracking-wide mr-3 px-32">
+                <h2 className=" items-center justify-center text-2xl text-center font-light tracking-wide mr-3 px-32">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam illo 
                     facilis adipisci dolorum eveniet laboriosam, dignissimos cumque ea aliquam itaque ducimus 
                     corporis tempora repellendus, maiores mollitia labore ipsa vero expedita. 
